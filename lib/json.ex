e0cd221697a7bb2a4cb7417e7123a198712fd184
nicriutman <bscastro@resuelvetudeuda.com>
2019-04-12 13:11:40 -0500

    calcular porcentajes de bono por jugador

--- lib/json.ex
+++ lib/json.ex
@@ -1,11 +1,18 @@
 defmodule Json do
 
   def main() do 
-    json = leer()
-    mapa_niveles = %{}
-    niveles = capturar_niveles(json, mapa_niveles)     
-    total_goles_individual = suma_goles_individuales(json, 0)   
-    meta_de_goles_grupal = suma_meta_de_goles_grupal(json, niveles, 0)    
+    json = 
+    leer()    
+    niveles = 
+    capturar_niveles(json, %{})     
+    total_goles_individual = 
+    suma_goles_individuales(json, 0)   
+    meta_de_goles_grupal = 
+    suma_meta_de_goles_grupal(json, niveles, 0)  
+    porcentaje_grupal = 
+    calcular_porcentaje_grupal(total_goles_individual, meta_de_goles_grupal)    
+    mapa_porcentaje_bono = 
+    calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles, %{})     
   end  
 
   defp capturar_niveles([], mapa_niveles), do: mapa_niveles
@@ -26,16 +33,33 @@ defmodule Json do
   defp suma_goles_individuales([], suma_goles), do: suma_goles
 
   defp suma_goles_individuales([h | t], suma_goles) do
-    suma_goles = suma_goles + h["goles"]
+    suma_goles = 
+    suma_goles + h["goles"]
     suma_goles_individuales(t, suma_goles)
   end
   
   defp suma_meta_de_goles_grupal([], niveles, meta), do: meta
 
   defp suma_meta_de_goles_grupal([h | t], niveles, meta) do
-    meta = niveles[h["nivel"]] + meta    
+    meta = 
+    niveles[h["nivel"]] + meta    
     suma_meta_de_goles_grupal(t, niveles, meta)
   end
+
+  defp calcular_porcentaje_bono_individual([], porcentaje_grupal, niveles, porcentajes), do: porcentajes
+
+  defp calcular_porcentaje_bono_individual([h | t], porcentaje_grupal, niveles, porcentajes) do    
+    porcentaje_individual =
+    (h["goles"]/niveles[h["nivel"]])
+    porcentajes = 
+    Map.put(porcentajes, h["nombre"], (porcentaje_individual+porcentaje_grupal)/2)
+    calcular_porcentaje_bono_individual(t, porcentaje_grupal, niveles, porcentajes)
+  end
+
+  defp calcular_porcentaje_grupal(total_goles_individual, meta_de_goles_grupal) do
+    porcentaje_grupal = 
+    total_goles_individual/meta_de_goles_grupal
+  end    
 
   defp leer() do     
        "#{Application.app_dir(:json)}/priv/" 
