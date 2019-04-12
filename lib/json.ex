4463912b9ebefd068fddaa8170432e5fffd91fbe
nicriutman <bscastro@resuelvetudeuda.com>
2019-04-12 11:24:22 -0500

    captura de niveles desde consola saltando los que ya estan

--- lib/json.ex
+++ lib/json.ex
@@ -1,11 +1,30 @@
 defmodule Json do
 
-  def main() do    
-    json = leer() 
+  def main() do 
+    json = leer()
+    mapa_niveles = %{}
+    niveles = 
+    json
+    |> capturar_niveles(mapa_niveles)     
     total_goles = 
     json
-    |> suma_goles_individuales(0)    
+    |> suma_goles_individuales(0)     
   end  
+
+  defp capturar_niveles([], mapa_niveles), do: mapa_niveles
+
+  defp capturar_niveles([h | t], mapa_niveles) do  
+    if Map.has_key?(mapa_niveles, h["nivel"]) do
+      capturar_niveles(t, mapa_niveles)
+    else
+      mapa_niveles = 
+      Map.put(mapa_niveles, h["nivel"],
+      IO.gets("ingrese un valor para el nivel #{inspect h["nivel"]}:")
+      |> Integer.parse()
+      |> elem(0))
+      capturar_niveles(t, mapa_niveles)
+    end      
+  end
 
   defp suma_goles_individuales([], suma_goles), do: suma_goles
 
