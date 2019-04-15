6d4f0e7d97a0b260d4f53b76cbcf89d40b46c8a9
nicriutman <bscastro@resuelvetudeuda.com>
2019-04-15 16:37:48 -0500

    eliminacion de recurcibidad

---
+++ .elixir_ls/build/test/lib/json/ebin/json.app
@@ -0,0 +1,6 @@
+{application,json,
+             [{applications,[kernel,stdlib,elixir,logger,poison]},
+              {description,"json"},
+              {modules,['Elixir.Json']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- lib/json.ex
+++ lib/json.ex
@@ -11,26 +11,27 @@ defmodule Json do
     IO.puts("#{inspect niveles}")
 
     total_goles_individual = 
-      suma_goles_individuales_por_equipo(json, %{})
-    IO.puts("#{inspect total_goles_individual} ")
+      suma_goles_individuales_por_equipo(json)
+    IO.puts("#{inspect total_goles_individual}")
 
     meta_de_goles_grupal = 
-      suma_meta_de_goles_grupal_por_equipo(json, niveles, %{}) 
-   
+      suma_meta_de_goles_grupal_por_equipo(json, niveles) 
+    IO.puts("#{inspect meta_de_goles_grupal}")
 
     porcentaje_grupal = 
-      calcular_porcentaje_grupal_por_equipo(total_goles_individual, meta_de_goles_grupal, %{}) 
-    
+      calcular_porcentaje_grupal_por_equipo(total_goles_individual, meta_de_goles_grupal) 
+      IO.puts("#{inspect porcentaje_grupal}")
 
     mapa_porcentaje_bono = 
-      calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles, %{})
-    
+      calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles)
+      IO.puts("#{inspect mapa_porcentaje_bono}")
     
     json_modificado =
-      modificar_json(json, mapa_porcentaje_bono, []) 
+      modificar_json(json, mapa_porcentaje_bono) 
 
     ver(json_modificado)   
   end  
+  
   
   defp leer() do     
        "#{Application.app_dir(:json)}/priv/" 
@@ -42,78 +43,68 @@ defmodule Json do
   defp capturar_niveles([], mapa_niveles), do: mapa_niveles
 
   defp capturar_niveles([cabeza | cola], mapa_niveles) do
-
-    if Map.has_key?(mapa_niveles, cabeza["nivel"]) do
-      
+    if Map.has_key?(mapa_niveles, cabeza["nivel"]) do    
       capturar_niveles(cola, mapa_niveles)
-
     else
-
       valor_nuevo_nivel =
         "ingrese un valor para el nivel #{cabeza["nivel"]}:"
         |> IO.gets()
         |> Integer.parse()
         |> elem(0)
-
       mapa_niveles = 
         Map.put(mapa_niveles, cabeza["nivel"], valor_nuevo_nivel)
-      
       capturar_niveles(cola, mapa_niveles)
-    
     end      
   end
 
 
-  defp suma_goles_individuales_por_equipo(json, suma_goles_por_equipo) do
-
-    suma_goles_por_equipo =
-      json
-      |> Enum.group_by(fn jugador -> jugador["equipo"]
-      end)
-      |> Enum.map(fn {equipo, jugadores} -> 
-      goles_equipo =
-        Enum.reduce(jugadores, 0, fn %{"goles" => goles} , acc -> 
-          acc + goles
+  defp suma_goles_individuales_por_equipo(json) do
+    
+    json
+    |> Enum.group_by(fn jugador -> jugador["equipo"]
+    end)
+    |> Enum.map(fn {equipo, jugadores} -> 
+    goles_equipo =
+      Enum.reduce(jugadores, 0, fn %{"goles" => goles} , acc -> 
+        acc + goles
       end)          
       %{equipo => goles_equipo}
-  end)
+    end)
+    |>  Enum.reduce( %{}, fn mapa, acc -> Map.merge(acc, mapa) end)
   end  
 
-  defp suma_meta_de_goles_grupal_por_equipo(json, niveles, meta) do          
-
-  end
-
-  defp calcular_porcentaje_grupal_por_equipo(total_goles_individual, meta_de_goles_grupal,porcentaje_grupal) do
+  defp suma_meta_de_goles_grupal_por_equipo(json, niveles) do
     
-    porcentaje_grupal = Map.merge(total_goles_individual, meta_de_goles_grupal, fn _kn, v1, v2 ->  v1 / v2 end)
-  
+    json
+    |> Enum.group_by(fn jugador -> jugador["equipo"]
+    end)
+    |> Enum.map(fn {equipo, jugadores} -> 
+      meta_de_goles_por_equipo =
+      Enum.reduce(jugadores, 0, fn %{"nivel" => nivel} , acc -> 
+        acc + niveles[nivel]
+      end)          
+    %{equipo => meta_de_goles_por_equipo}
+    end)
+    |>  Enum.reduce( %{}, fn mapa, acc -> Map.merge(acc, mapa) end)
   end
 
-  defp calcular_porcentaje_bono_individual([], porcentaje_grupal, niveles, porcentajes), do: porcentajes
-
-  defp calcular_porcentaje_bono_individual([cabeza | cola], porcentaje_grupal, niveles, porcentajes) do    
-    porcentaje_individual =
-    (cabeza["goles"]/niveles[cabeza["nivel"]])
-    porcentajes = 
-     Map.put(porcentajes, cabeza["nombre"], (porcentaje_individual+porcentaje_grupal[cabeza["equipo"]])/2)
-    calcular_porcentaje_bono_individual(cola, porcentaje_grupal, niveles, porcentajes)
+  defp calcular_porcentaje_grupal_por_equipo(total_goles_por_equipo, meta_de_goles_por_equipo) do
+    Map.merge(total_goles_por_equipo, meta_de_goles_por_equipo, fn _kn, v1, v2 ->  v1 / v2 end)  
   end
 
-  defp modificar_json([], _, nuevo_json), do: nuevo_json
-
-  defp modificar_json([cabeza | cola], mapa_porcentaje_bono, nuevo_json) do
-    bono =
-    cabeza["bono"]*mapa_porcentaje_bono[cabeza["nombre"]] 
-    nuevo_json =
-    nuevo_json ++ [Map.replace!(cabeza, "sueldo_completo", bono + cabeza["sueldo"])]
-    modificar_json(cola, mapa_porcentaje_bono, nuevo_json)
+  defp calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles) do    
+    Enum.reduce(json, %{}, fn jugador, acc -> 
+      Map.merge(acc, %{jugador["nombre"] => 
+        ((jugador["goles"]/niveles[jugador["nivel"]])+porcentaje_grupal[jugador["equipo"]])/2}) end)    
   end
-  
-  defp ver([]), do: "listo"
 
-  defp ver([cabeza | cola]) do
-    IO.puts("#{inspect cabeza}")
-    ver(cola)
+  defp modificar_json(json, mapa_porcentaje_bono) do
+    Enum.map(json, fn jugadores -> Map.replace!(jugadores, "sueldo_completo",
+      (jugadores["bono"] * mapa_porcentaje_bono[jugadores["nombre"]]) + jugadores["sueldo"])end)    
+  end
+
+  defp ver(json) do
+    Enum.map(json, fn jugador -> IO.puts("#{inspect jugador}")end)
   end
 
 end
