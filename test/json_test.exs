e403643c19e765bde11a9a3089f6cef8fc2ad36b
nicriutman <bscastro@resuelvetudeuda.com>
2019-04-15 17:52:30 -0500

    agrego test inicial

--- test/json_test.exs
+++ test/json_test.exs
@@ -1,8 +1,33 @@
 defmodule JsonTest do
   use ExUnit.Case
-  doctest Json
+  import Mock
+  alias Json
+  require Logger
 
-  test "greets the world" do
-    assert Json.hello() == :world
+  
+  test "deveria agregar nuevos niveles" do
+    with_mocks( 
+      [ 
+        { 
+          IO, 
+          [], 
+          [ 
+            gets: fn _ -> "25\n" end 
+          ] 
+        } 
+      ] 
+    ) do         
+    niveles_por_default =
+      %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
+    json_de_prueba = 
+      [%{"nivel" => "prueba1"},%{"nivel" => "prueba2"},%{"nivel" => "prueba3"}]
+      Logger.error("#{inspect Json.__info__(:functions)}")
+      resultado =
+      Json.capturar_niveles(json_de_prueba, niveles_por_default)
+      esperado =
+      %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20, "prueba1" => 25, "prueba2" => 25, "prueba3" => 25}
+      assert esperado == resultado
+      end
   end
+  
 end
