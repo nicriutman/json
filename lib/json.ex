98aa35e49847f3a2869bf555b6ec3c1d054e1f8e
nicriutman <bscastro@resuelvetudeuda.com>
2019-04-15 19:39:54 -0500

    agregar test

--- lib/json.ex
+++ lib/json.ex
@@ -1,42 +1,49 @@
 defmodule Json do
+
   @niveles_por_defecto %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
 
-  def inicio() do
-    json = leer()
+  def inicio() do 
+    json = 
+      leer()
 
-    niveles = capturar_niveles(json, @niveles_por_defecto)
-    IO.puts("#{inspect(niveles)}")
+    niveles = 
+      capturar_niveles(json, @niveles_por_defecto)
+    IO.puts("#{inspect niveles}")
 
-    total_goles_individual = suma_goles_individuales_por_equipo(json)
-    IO.puts("#{inspect(total_goles_individual)}")
+    total_goles_individual = 
+      suma_goles_individuales_por_equipo(json)
+    IO.puts("#{inspect total_goles_individual}")
 
-    meta_de_goles_grupal = suma_meta_de_goles_grupal_por_equipo(json, niveles)
-    IO.puts("#{inspect(meta_de_goles_grupal)}")
+    meta_de_goles_grupal = 
+      suma_meta_de_goles_grupal_por_equipo(json, niveles) 
+    IO.puts("#{inspect meta_de_goles_grupal}")
 
-    porcentaje_grupal =
-      calcular_porcentaje_grupal_por_equipo(total_goles_individual, meta_de_goles_grupal)
+    porcentaje_grupal = 
+      calcular_porcentaje_grupal_por_equipo(total_goles_individual, meta_de_goles_grupal) 
+      IO.puts("#{inspect porcentaje_grupal}")
 
-    IO.puts("#{inspect(porcentaje_grupal)}")
+    mapa_porcentaje_bono = 
+      calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles)
+      IO.puts("#{inspect mapa_porcentaje_bono}")
+    
+    json_modificado =
+      modificar_json(json, mapa_porcentaje_bono) 
 
-    mapa_porcentaje_bono = calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles)
-    IO.puts("#{inspect(mapa_porcentaje_bono)}")
-
-    json_modificado = modificar_json(json, mapa_porcentaje_bono)
-
-    ver(json_modificado)
+    ver(json_modificado)   
+  end  
+  
+  
+  def leer() do     
+       "#{Application.app_dir(:json)}/priv/" 
+       |> Path.join("json.json") 
+       |> File.read!() 
+       |> Poison.decode!()        
   end
 
-  defp leer() do
-    "#{Application.app_dir(:json)}/priv/"
-    |> Path.join("json.json")
-    |> File.read!()
-    |> Poison.decode!()
-  end
+  def capturar_niveles([], mapa_niveles), do: mapa_niveles
 
-  defp capturar_niveles([], mapa_niveles), do: mapa_niveles
-
-  defp capturar_niveles([cabeza | cola], mapa_niveles) do
-    if Map.has_key?(mapa_niveles, cabeza["nivel"]) do
+  def capturar_niveles([cabeza | cola], mapa_niveles) do
+    if Map.has_key?(mapa_niveles, cabeza["nivel"]) do    
       capturar_niveles(cola, mapa_niveles)
     else
       valor_nuevo_nivel =
@@ -44,65 +51,71 @@ defmodule Json do
         |> IO.gets()
         |> Integer.parse()
         |> elem(0)
-
-      mapa_niveles = Map.put(mapa_niveles, cabeza["nivel"], valor_nuevo_nivel)
+      mapa_niveles = 
+        Map.put(mapa_niveles, cabeza["nivel"], valor_nuevo_nivel)
       capturar_niveles(cola, mapa_niveles)
-    end
+    end      
   end
 
-  defp suma_goles_individuales_por_equipo(json) do
-    json
-    |> Enum.group_by(fn jugador -> jugador["equipo"] end)
-    |> Enum.map(fn {equipo, jugadores} ->
-      goles_equipo =
-        Enum.reduce(jugadores, 0, fn %{"goles" => goles}, acc ->
-          acc + goles
-        end)
 
+  def suma_goles_individuales_por_equipo(json) do
+    
+    json
+    |> Enum.group_by(fn jugador -> jugador["equipo"]
+    end)
+    |> Enum.map(fn {equipo, jugadores} -> 
+    goles_equipo =
+      Enum.reduce(jugadores, 0, fn %{"goles" => goles} , acc -> 
+        acc + goles
+      end)          
       %{equipo => goles_equipo}
     end)
-    |> Enum.reduce(%{}, fn mapa, acc -> Map.merge(acc, mapa) end)
-  end
+    |>  Enum.reduce( %{}, fn mapa, acc -> Map.merge(acc, mapa) end)
+  end  
 
-  defp suma_meta_de_goles_grupal_por_equipo(json, niveles) do
+  def suma_meta_de_goles_grupal_por_equipo(json, niveles) do
+    
     json
-    |> Enum.group_by(fn jugador -> jugador["equipo"] end)
-    |> Enum.map(fn {equipo, jugadores} ->
+    |> Enum.group_by(fn jugador -> jugador["equipo"]
+    end)
+    |> Enum.map(fn {equipo, jugadores} -> 
       meta_de_goles_por_equipo =
-        Enum.reduce(jugadores, 0, fn %{"nivel" => nivel}, acc ->
-          acc + niveles[nivel]
-        end)
-
-      %{equipo => meta_de_goles_por_equipo}
+      Enum.reduce(jugadores, 0, fn %{"nivel" => nivel} , acc -> 
+        acc + niveles[nivel]
+      end)          
+    %{equipo => meta_de_goles_por_equipo}
     end)
-    |> Enum.reduce(%{}, fn mapa, acc -> Map.merge(acc, mapa) end)
+    |>  Enum.reduce( %{}, fn mapa, acc -> Map.merge(acc, mapa) end)
   end
 
-  defp calcular_porcentaje_grupal_por_equipo(total_goles_por_equipo, meta_de_goles_por_equipo) do
-    Map.merge(total_goles_por_equipo, meta_de_goles_por_equipo, fn _kn, v1, v2 -> v1 / v2 end)
+  def calcular_porcentaje_grupal_por_equipo(total_goles_por_equipo, meta_de_goles_por_equipo) do
+    Map.merge(total_goles_por_equipo, meta_de_goles_por_equipo, fn _kn, v1, v2 -> 
+      v1
+      |> Kernel./(v2)
+      |> Float.round(2)
+    end)  
   end
 
-  defp calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles) do
-    Enum.reduce(json, %{}, fn jugador, acc ->
-      Map.merge(acc, %{
-        jugador["nombre"] =>
-          (jugador["goles"] / niveles[jugador["nivel"]] + porcentaje_grupal[jugador["equipo"]]) /
-            2
-      })
-    end)
+  def calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles) do    
+    Enum.reduce(json, %{}, fn %{"goles" => goles, "nivel" => nivel, "equipo" => equipo, "nombre" => nombre}, acc -> 
+      individual =
+        goles
+        |> Kernel./(niveles[nivel])
+        |> Kernel.+(porcentaje_grupal[equipo])
+        |> Kernel./(2)
+        |> Float.round(2)     
+      Map.merge(acc, %{nombre => individual}) 
+    end)    
   end
 
-  defp modificar_json(json, mapa_porcentaje_bono) do
-    Enum.map(json, fn jugadores ->
-      Map.replace!(
-        jugadores,
-        "sueldo_completo",
-        jugadores["bono"] * mapa_porcentaje_bono[jugadores["nombre"]] + jugadores["sueldo"]
-      )
-    end)
+  def modificar_json(json, mapa_porcentaje_bono) do
+    Enum.map(json, fn jugadores -> Map.replace!(jugadores, "sueldo_completo", 
+      (jugadores["bono"] * mapa_porcentaje_bono[jugadores["nombre"]]) + jugadores["sueldo"])
+    end)    
   end
 
-  defp ver(json) do
-    Enum.map(json, fn jugador -> IO.puts("#{inspect(jugador)}") end)
+  def ver(json) do
+    Enum.map(json, fn jugador -> IO.puts("#{inspect jugador}")end)
   end
+
 end
--- test/json_test.exs
+++ test/json_test.exs
@@ -20,8 +20,7 @@ defmodule JsonTest do
     niveles_por_default =
       %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
     json_de_prueba = 
-      [%{"nivel" => "prueba1"},%{"nivel" => "prueba2"},%{"nivel" => "prueba3"}]
-      Logger.error("#{inspect Json.__info__(:functions)}")
+      [%{"nivel" => "prueba1"},%{"nivel" => "prueba2"},%{"nivel" => "prueba3"}]      
       resultado =
       Json.capturar_niveles(json_de_prueba, niveles_por_default)
       esperado =
@@ -30,4 +29,79 @@ defmodule JsonTest do
       end
   end
   
+  test "debe sumar los goles individuales de cada equipo" do       
+    json_de_prueba =
+    "#{Application.app_dir(:json)}/priv/" 
+    |> Path.join("json.json") 
+    |> File.read!() 
+    |> Poison.decode!()
+      resultado =
+      Json.suma_goles_individuales_por_equipo(json_de_prueba)
+      esperado =
+      %{"azul" => 37, "rojo" => 19}
+      assert esperado == resultado
+  end
+
+  test "debe sumar la meta de goles que deben meter" do       
+    json_de_prueba =
+      "#{Application.app_dir(:json)}/priv/" 
+      |> Path.join("json.json") 
+      |> File.read!() 
+      |> Poison.decode!() 
+    niveles_por_default =
+     %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
+    resultado =
+      Json.suma_meta_de_goles_grupal_por_equipo(json_de_prueba, niveles_por_default)
+    esperado =
+      %{"azul" => 25, "rojo" => 25}
+    assert esperado == resultado
+  end
+
+  test "calcula el porcentaje de goles metidos por equipo" do       
+    total_goles_por_equipo = 
+      %{"azul" => 37, "rojo" => 19}
+    meta_de_goles_por_equipo =
+      %{"azul" => 25, "rojo" => 25}
+    resultado =
+      Json.calcular_porcentaje_grupal_por_equipo(total_goles_por_equipo, meta_de_goles_por_equipo)
+    esperado =
+      %{"azul" => 1.48, "rojo" => 0.76}
+    assert esperado == resultado
+  end
+
+  test "calcula el bono de cada jugador" do
+    json =
+      "#{Application.app_dir(:json)}/priv/" 
+      |> Path.join("json.json") 
+      |> File.read!() 
+      |> Poison.decode!()       
+    porcentaje_grupal = 
+      %{"azul" => 1.48, "rojo" => 0.76}
+    niveles = 
+      %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
+    resultado =
+      Json.calcular_porcentaje_bono_individual(json, porcentaje_grupal, niveles)
+    esperado =
+    %{"Cosme Fulanito" => 1.44, "EL Cuauh" => 1.49, "El Rulo" => 0.83, "Juan Perez" => 0.71}
+    assert esperado == resultado
+  end
+  
+  test "modifica el valor de la clave sueldo_completo" do
+    json =
+      "#{Application.app_dir(:json)}/priv/" 
+      |> Path.join("json.json") 
+      |> File.read!() 
+      |> Poison.decode!()       
+    porcentaje_de_bono =
+      %{"Cosme Fulanito" => 1.44, "EL Cuauh" => 1.49, "El Rulo" => 0.8300000000000001, "Juan Perez" => 0.7133333333333334}
+    resultado =
+      Json.modificar_json(json, porcentaje_de_bono)
+    esperado =
+    [%{"bono" => 25000, "equipo" => "rojo", "goles" => 10, "nivel" => "C", "nombre" => "Juan Perez", "sueldo" => 50000, "sueldo_completo" => 67833.33333333334}, 
+    %{"bono" => 30000, "equipo" => "azul", "goles" => 30, "nivel" => "Cuauh", "nombre" => "EL Cuauh", "sueldo" => 100000, "sueldo_completo" => 1.447e5}, 
+    %{"bono" => 10000, "equipo" => "azul", "goles" => 7, "nivel" => "A", "nombre" => "Cosme Fulanito", "sueldo" => 20000, "sueldo_completo" => 3.44e4}, 
+    %{"bono" => 15000, "equipo" => "rojo", "goles" => 9, "nivel" => "B", "nombre" => "El Rulo", "sueldo" => 30000, "sueldo_completo" => 42450.0}]
+    assert esperado == resultado
+  end    
+
 end
